4df7f844e64bd4ee357df8c271e1d7b82678919e
Nikola Spalevic <nikola.s@vivifyideas.com>
2018-05-15 19:41:34 +0200

    Commit 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import HelloWorld from './components/HelloWorld'
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: HelloWorld },
+]
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
